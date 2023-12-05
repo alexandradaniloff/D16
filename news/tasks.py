92fbbd45c43e820b597e9d5f4a4e70d87370e480
Alexander <alexandradaniloff@gmail.ru>
2023-12-05 23:02:02 +0300

    celery2

---
+++ news/tasks.py
@@ -0,0 +1,59 @@
+from celery import shared_task
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from .models import Post, Category
+import datetime
+
+@shared_task
+def send_email_task(pk):
+    post = Post.objects.get(pk=pk)
+    categories = post.categories.all()
+    title = post.title
+    subscribers_emails = []
+    for category in categories:
+        subscribers = category.subscribers.all()
+        subscribers_emails += [s.email for s in subscribers]
+
+    html_content = render_to_string(
+        'post_created_email.html',
+        {
+            'text': f'{post.title[0:50]}',
+            'link': f'{settings.SITE_URL}/news/{pk}'
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject=title,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers_emails,
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+
+@shared_task
+def weekly_send_email_task():
+    today = datetime.now()
+    last_week = today - timedelta(days=7)
+    posts = Post.objects.filter(date_create__gte=last_week)
+
+    categories = set(posts.values_list('categories__name', flat=True))
+    subscribers = set(Category.objects.filter(name__in=categories).values_list('subscribers__email', flat=True))
+
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
---
+++ project/celery.py
@@ -0,0 +1,19 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+app = Celery('project')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'action_every_monday_8am':{
+        'task': 'news.tasks.weekly_send_email_task',
+        'schedule': crontab(),
+        #срабатывание 1 раз в минуту
+        'args':()
+    }
+}
