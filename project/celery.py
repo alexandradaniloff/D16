0abf0a0a40da0d1b760d36b9cd3abb9327f78ae0
Alexander <alexandradaniloff@gmail.ru>
2023-12-16 14:54:35 +0300

    D10 итог

--- project/celery.py
+++ project/celery.py
@@ -13,10 +13,13 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'action_every_monday_8am':{
         'task': 'news.tasks.weekly_send_email_task',
-        'schedule': crontab(),
+        #'schedule': crontab(),
         #срабатывание 1 раз в минуту
-        #'schedule': crontab(hour=8, minute=0, day_of_week='saturday'),
         #'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+        'schedule': crontab(hour=11, minute=50, day_of_week='saturday'),
         'args':()
     }
 }
+app.conf.timezone = 'UTC'
+# реальное время (UTC 3) больше на 3часа , чем (UTC)
+app.autodiscover_tasks()
