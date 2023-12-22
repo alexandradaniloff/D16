7c6cde8f469de8a171213922b380ae6f524ff3dd
Alexander <alexandradaniloff@gmail.ru>
2023-12-22 15:22:18 +0300

    D14

--- news/admin.py
+++ news/admin.py
@@ -1,13 +1,23 @@
+
 from django.contrib import admin
+from .models import (
+    Post, Category, PostCategory,
+)
+class CategoryInline(admin.TabularInline):
+    model = PostCategory
+    extra = 1
+class PostAdmin(admin.ModelAdmin):
+    inlines = (CategoryInline, )
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('date_create', 'title', 'content', 'post_type', 'categories')
+    list_filter = ('date_create', 'title', 'content', 'post_type')
+    search_fields = ('date_create', 'title', 'content', 'post_type')
 
-# Register your models here.
-from django.contrib import admin
-
-from .models import Post, Category, Author, Comment
-
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Author)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin, )
+admin.site.register(PostCategory,)
+admin.site.register(Category,)
 admin.site.register(Post.categories.through)
+#admin.site.register(Author)
+#admin.site.register(Comment)
+
+
