3476932fb9e415dbc023c3747d8bd53add460c82
Alexander <alexandradaniloff@gmail.ru>
2023-12-23 17:53:56 +0300

    D14 с выводом категорий

--- news/admin.py
+++ news/admin.py
@@ -1,23 +1,22 @@
 
 from django.contrib import admin
 from .models import (
-    Post, Category, PostCategory,
+    Post, Category,
 )
-class CategoryInline(admin.TabularInline):
-    model = PostCategory
-    extra = 1
+
 class PostAdmin(admin.ModelAdmin):
-    inlines = (CategoryInline, )
+    def category(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
-    list_display = ('date_create', 'title', 'content', 'post_type')
-    list_filter = ('date_create', 'title', 'content', 'post_type')
-    #search_fields = ('date_create', 'title', 'content', 'post_type')
+    list_display = ('date_create', 'title', 'content', 'post_type', 'category')
+    #list_filter = ('date_create', 'title', 'content', 'post_type')
+    search_fields = ('date_create', 'title', 'content', 'post_type')
+
+
 
 admin.site.register(Post, PostAdmin, )
-admin.site.register(PostCategory,)
-admin.site.register(Category,)
-admin.site.register(Post.categories.through)
-#admin.site.register(Author)
-#admin.site.register(Comment)
+admin.site.register(Category, )
+#admin.site.register(Post.categories.through)
+
 
 
